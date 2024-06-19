94cb7a346858628f487a3be1de08ced5b5f74dd9
fazzaamiarso <fazzaamiarso@gmail.com>
2024-06-20 02:24:46 +0700

    chore: move site configurations to own file

---
+++ src/site-config.ts
@@ -0,0 +1,19 @@
+export const siteTitleBase = "Fazza Razaq Amiarso";
+export const siteTitle = `${siteTitleBase} | Frontend Developer & Mentor`;
+export const siteDescription =
+  "A frontend developer with focus on making accessible software with beautiful interfaces for everyone";
+export const siteUrl = "fazzaamiarso.com";
+
+export const ogImageUrl =
+  "https://res.cloudinary.com/dkiqn0gqg/image/upload/v1693350354/fazzaamiarso-og-final.png";
+export const ogImageAlt = "Fazza Razaq Amiarso portfolio banner";
+
+export const twitterHandle = "@faz_razq";
+export const email = "fazzamiarso@gmail.com";
+
+export const linkedinUrl = "https://www.linkedin.com/in/fazzaamiarso/";
+export const githubUrl = "https://github.com/fazzaamiarso";
+export const twitterUrl = "https://twitter.com/faz_razq";
+export const blogUrl = "https://dev.to/fazzaamiarso";
+export const resumeUrl =
+  "https://drive.google.com/file/d/1qPe56ZpyYuZLJB7V4KjMKB1TsnFioI7w/view?usp=drive_link";
