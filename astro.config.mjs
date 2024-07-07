8ffc5141e250e089a0213e32f64322d81eab1bfd
fazzaamiarso <fazzaamiarso@gmail.com>
2024-07-07 19:33:34 +0700

    feat: add lightbox to image gallery in project detail

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,23 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
-
 import rehypeWrap from "rehype-wrap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fazzaamiarso.com",
   markdown: {
-    rehypePlugins: [
-      [rehypeWrap, { wrapper: "div.image-wrapper", selector: "img", fallback: false }],
-    ],
+    rehypePlugins: [[rehypeWrap, {
+      wrapper: "div.image-wrapper",
+      selector: "img",
+      fallback: false
+    }]]
   },
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), mdx()],
   output: "static",
-  adapter: vercel(),
+  adapter: vercel()
 });
