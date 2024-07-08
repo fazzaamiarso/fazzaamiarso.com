0aa07d5e8bd8d92f3dfd8ddd7a3fd632e1daf21a
fazzaamiarso <fazzaamiarso@gmail.com>
2024-07-09 01:11:48 +0700

    feat: add video player and improve lightbox

--- src/components/project/split-image.tsx
+++ src/components/project/split-image.tsx
@@ -1,26 +1,31 @@
 import { useState, type ReactNode } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
 
 export default function SplitImage({ children }: { children: ReactNode }) {
   return <div className="grid grid-cols-2 items-start gap-4">{children}</div>;
 }
 
 export function SplitContent({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <div className="flex flex-col space-y-4">{children}</div>
-    </>
-  );
+  return <div className="flex flex-col gap-4">{children}</div>;
 }
 
 export function SplitItem({ src }: { src: string }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div>
+    <div className="">
       <img src={src} alt="" className="w-full" onClick={() => setIsOpen(true)} />
-      {isOpen && <Lightbox open={isOpen} close={() => setIsOpen(false)} slides={[{ src }]} />}
+      {isOpen && (
+        <Lightbox
+          plugins={[Zoom]}
+          open={isOpen}
+          close={() => setIsOpen(false)}
+          slides={[{ src }]}
+          render={{ buttonNext: () => null, buttonPrev: () => null }}
+        />
+      )}
     </div>
   );
 }
