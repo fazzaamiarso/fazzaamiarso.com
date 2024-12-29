c71dc255596e550466246f691244bb55e2a0f833
fazzaamiarso <fazzaamiarso@gmail.com>
2024-12-29 17:55:39 +0700

    chore: update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 import rehypeWrap from "rehype-wrap";
 
 import mdx from "@astrojs/mdx";
